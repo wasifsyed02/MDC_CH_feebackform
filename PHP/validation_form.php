e4684f1d2f997da96ceea18c0446f94feca04a65
syed wasif <wasifsyed02>
2020-07-04 18:29:37 +0530

    added function to prevent sqlinjection

--- PHP/validation_form.php
+++ PHP/validation_form.php
@@ -2,15 +2,16 @@
 include("DB.php");
 function verify_user($input,$input2){
     $boolean=true;
-        $username=test_input($input);
-        $password=test_input($input2);
+    global $conn;
+        $username=mysqli_real_escape_string($conn,test_input($input));
+        $password=mysqli_real_escape_string($conn,test_input($input2));
          if(empty($username))
               $boolean=false;
         if(empty($password))
              $boolean=false;
         if($boolean==true)
         {
-            global $conn;
+           
             $sql="select * from user_table where username='$username' && Password='$password'";
             $result=mysqli_query($conn,$sql);
             if(mysqli_num_rows($result)>0)
